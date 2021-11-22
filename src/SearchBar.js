4215bcf7909653b0f6e985d2cff4b1af3522bb50
Gavriel Frant <gavril44@gmail.com>
2021-11-22 22:28:45 +0200

    added style and littel bug fixes

--- src/SearchBar.js
+++ src/SearchBar.js
@@ -1,9 +1,9 @@
 import "./SearchBar.css";
 import Input from "./components/Input";
 import OptionsDropdown from "./components/OptionsDropdown";
-import { useRef, useEffect, useState, useReducer } from "react";
+import { useRef, useEffect, useState } from "react";
 import countriesJson from "./countries";
-import useFilteredJson from "./components/useFilteredJson";
+import useFilteredJson from "./hooks/useFilteredJson";
 
 function SearchBar() {
   const dropdownElementRef = useRef(null);
@@ -34,19 +34,48 @@ function SearchBar() {
 
   return (
     <div className="SearchBar">
-      <Input
-        inputRef={inputElementRef}
-        value={value}
-        onMouseDown={() => {
-          dropdownElementRef.current.style.display = "";
-        }}
-        onChange={handleChange}
-        onBlur={() => {
-          console.log("blur");
-          dropdownElementRef.current.style.display = "none";
-          setFilteredCountries(countriesJson);
-        }}
-      />
+      <div className="input-section">
+        <Input
+          inputRef={inputElementRef}
+          value={value}
+          onMouseDown={() => {
+            dropdownElementRef.current.style.display = "";
+          }}
+          onChange={handleChange}
+          onBlur={() => {
+            console.log("blur");
+            setValue("");
+            dropdownElementRef.current.style.display = "none";
+          }}
+        />
+        {value === "" ? null : (
+          <button
+            id="remove"
+            onClick={() => {
+              clickHandler("");
+            }}
+            onMouseDown={(e) => {
+              e.preventDefault();
+            }}
+          >
+            X
+          </button>
+        )}
+        <button
+          id="toggle"
+          onMouseDown={(e) => {
+            e.preventDefault();
+          }}
+          onClick={() => {
+            const currentDisplay = dropdownElementRef.current.style.display;
+            dropdownElementRef.current.style.display =
+              currentDisplay === "" ? "none" : "";
+            inputElementRef.current.focus();
+          }}
+        >
+          Toggle
+        </button>
+      </div>
 
       <div ref={dropdownElementRef}>
         <OptionsDropdown
@@ -54,28 +83,6 @@ function SearchBar() {
           clickHandler={clickHandler}
         />
       </div>
-      {value === "" ? null : (
-        <button
-          onClick={() => {
-            clickHandler("");
-          }}
-          onMouseDown={(e) => {
-            e.preventDefault();
-          }}
-        >
-          X
-        </button>
-      )}
-      <button
-        onClick={() => {
-          const currentDisplay = dropdownElementRef.current.style.display;
-          dropdownElementRef.current.style.display =
-            currentDisplay === "" ? "none" : "";
-          inputElementRef.current.focus();
-        }}
-      >
-        Toggle
-      </button>
     </div>
   );
 }
